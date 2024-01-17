908ac47d6a2fb6dfce0120d79e06220101faf46e
leanne1102 <leanneagiramukama@gmail.com>
2024-01-17 09:27:12 -0800

    onchange.html

---
+++ index.js
@@ -0,0 +1,87 @@
+// console.log("hello world")
+// let h = 40
+
+// console.log(h)
+// const herBeautifulName=`Leanne Agiramukama lives in Lweza.`
+// console.log(herBeautifulName)
+// let purple =1102
+// console.log(purple)
+
+// var food =12
+// let drink =10
+// const meal = food + drink
+// console.log(meal)
+
+// var pink ='Leanne'
+// var blue ='Agira'
+// var purple =pink+blue
+// console.log(purple)
+// let eggs ="2"
+// let cake ="6"
+// let milk = eggs + cake
+// console.log(milk)
+
+// const school =28
+// const teacher =8
+// const me = school - teacher
+// console.log(me)
+// let y=30
+// ley x=2
+// let z=30*2
+// console.log(z)
+// let g=40
+// let h=2
+// let i=40/2
+// console.log(i)
+// var x=10
+// x++
+
+
+// console.log(x)
+
+// var y=1
+// y--
+// console.log(y)
+// var d=4
+// var y=2
+// var x=d**y
+// console.log(x)
+// var j=10
+// var k=3
+// var t=j%k
+
+//  console.log(`4`=== 4)
+
+
+// var y=10
+// y +=8
+// console.log(y)
+
+// function toasting(){
+//     console.log ("roast the bread medium to well")
+// }
+// toasting()
+// function ironing(){
+//     console.log("produce heat and steam until maximum")
+
+// }
+// ironing()
+// function addingTwoNumbers(firstNumber,secondNumber){
+//     return firstNumber+secondNumber
+// }
+// console.log(addingTwoNumbers(500,2))
+
+// function dividingTwoNumbers(firstNumber,secondNumber){
+//     return firstNumber/secondNumber
+// }
+// console.log(dividingTwoNumbers(4,2))
+
+// function addingThreeNumbers(firstNumber,secondNumber,thirdNumber){
+//     return firstNumber+secondNumber+thirdNumber
+// }
+// console.log(addingThreeNumbers(1,1,1))
+
+var shirt ={type:"crop top",colour:"beige",material:"silk"}
+console.log(shirt.type)
+console.log(shirt.colour)
+console.log(shirt.material)
---
+++ new.js
@@ -0,0 +1,18 @@
+function Leanne(){
+    console.log("hello,you're most welcome to this website.")
+}
+function sama(){
+    console.log("my favorite colours are purple,grey and beige.")
+}
+function Agira(){
+    console.log("that is the direction to my home.")
+}
+function skyla(){
+    console.log("am the developer...others cant see this.")
+}
+function zoe(){
+    console.log("your welcome to my website.")
+}
+function samalie(){
+    console.log("The school is found  in Kitende along Entebbe road.")
+}
